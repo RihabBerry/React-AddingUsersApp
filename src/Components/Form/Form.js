a3c8833c60cc0f399f955dc18f092ef58daebc2e
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-22 16:49:23 +0200

    Added Modal

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,32 +1,44 @@
-import { React, useState } from "react";
+import { Fragment, React, useState } from "react";
 import classes from "./Form.module.css";
-
+import Modal from "../UI/Modal";
 const Form = (props) => {
   const [age, setAge] = useState("");
   const [name, setName] = useState("");
 
-  const AgeHandler = (event) => {
+  const ageHandler = (event) => {
+    if (event.target.value === "" || event.target.value === 0) {
+      return;
+    }
     setAge(event.target.value);
   };
-  const NameHandler = (event) => {
+  const nameHandler = (event) => {
+    if (event.target.value === "") {
+      return;
+    }
     setName(event.target.value);
   };
   const onSubmitHandler = (event) => {
     event.preventDefault();
     let user = name + age;
     props.AddUser(user);
+    setName("");
+    setAge("");
   };
 
   return (
-    <div className={classes.container}>
-      <form onSubmit={onSubmitHandler}>
-        <label>User Name</label>
-        <input onChange={NameHandler} />
-        <label>User Age</label>
-        <input onChange={AgeHandler} />
-        <button type="submit">Add user</button>
-      </form>
-    </div>
+    <Fragment>
+      <Modal show={show} />
+
+      <div className={classes.container}>
+        <form onSubmit={onSubmitHandler}>
+          <label>User Name</label>
+          <input onChange={nameHandler} value={name} />
+          <label>User Age</label>
+          <input onChange={ageHandler} value={age} />
+          <button type="submit">Add user</button>
+        </form>
+      </div>
+    </Fragment>
   );
 };
 export default Form;
--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,13 +1,31 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
+import classes from "./Modal.module.css";
 
 const Modal = (props) => {
   if (!props.show) {
     return null;
   }
   return (
-    <Fragment>
-      <p> This is Modal</p>
-    </Fragment>
+    <div className={classes.backdrop}>
+      <div className={classes.wrapper}>
+        <header className={classes.header}>
+          <p>Title</p>
+          <span>X</span>
+        </header>
+        <main className={classes.main}>
+          <p>
+            This is the content of the header hez ndddThis is the content of the
+            header hez ndddThis is the content of the header hez ndddThis is the
+            content of the header hez ndddThis is the content of the header hez
+            ndddThis is the content of the header hez ndddThis is the content of
+            the header hez ndddThis is the content of the header hez nddd
+          </p>
+        </main>
+        <footer className={classes.footer}>
+          <button>Close</button>
+        </footer>
+      </div>
+    </div>
   );
 };
 export default Modal;
